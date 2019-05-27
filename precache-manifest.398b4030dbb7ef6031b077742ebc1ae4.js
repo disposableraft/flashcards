ebd346283f65b6b162ccd09804289ccb28cd606e
Lance W <lwakeling@gmail.com>
2019-05-27 18:18:54 -0400

    Updates

---
+++ precache-manifest.398b4030dbb7ef6031b077742ebc1ae4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87dd7c020e00b2cad3f0126d1825d59f",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "6cd74507bc361e7f9f05",
+    "url": "/flashcards/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d9bb28b555ed1eda6e6e",
+    "url": "/flashcards/static/js/2.4e5c4a70.chunk.js"
+  },
+  {
+    "revision": "6cd74507bc361e7f9f05",
+    "url": "/flashcards/static/js/main.78453b4f.chunk.js"
+  },
+  {
+    "revision": "3afa89cd04d892312605",
+    "url": "/flashcards/static/js/runtime~main.4a72e263.js"
+  }
+]);
