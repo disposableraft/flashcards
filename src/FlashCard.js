47c8419b887faa8abaf1abf4336e20916b5796d1
Lance W <50839441+disposableraft@users.noreply.github.com>
2019-05-31 18:17:15 -0400

    Show the image on a background

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -10,11 +10,17 @@ function FlashCard(props) {
     multipleChoices,
   } = props;
 
+  const backgroundImage = {
+    background: `center / contain url(${multipleChoices[correctIndex].image}?width=500)`,
+    width: '500px',
+    height: '500px',
+  };
+
   switch (gameState) {
     case 'playing':
       return (
         <div>
-          <p><img src={multipleChoices[correctIndex].image + '?width=500'} alt='What is this mushroom?' /></p>
+          <div style={backgroundImage} />
           <button style={guessed[0] ? { backgroundColor: 'red' } : null} onClick={() => handleMakeAGuess(0)}>{multipleChoices[0].taxonName}</button>
           <button style={guessed[1] ? { backgroundColor: 'red' } : null} onClick={() => handleMakeAGuess(1)}>{multipleChoices[1].taxonName}</button>
           <button style={guessed[2] ? { backgroundColor: 'red' } : null} onClick={() => handleMakeAGuess(2)}>{multipleChoices[2].taxonName}</button>
@@ -23,7 +29,7 @@ function FlashCard(props) {
     case 'next':
       return (
         <div>
-          <p><img src={multipleChoices[correctIndex].image + '?width=500'} alt='What is this mushroom?' /></p>
+          <div style={backgroundImage} />
           <p>Score: {props.score}</p>
           <p>Yep, the answer is <a target="_blank" rel="noopener noreferrer" href={multipleChoices[correctIndex].item}>{multipleChoices[correctIndex].taxonName}</a>.</p>
           <p><button onClick={() => advanceToNextCard()} >Next</button></p>
