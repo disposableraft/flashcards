1364501c1c4389ed2ecf6557d7d4bc2415c7ed73
Lance W <50839441+disposableraft@users.noreply.github.com>
2019-06-02 22:43:31 -0400

    Responsive grid

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -20,20 +20,28 @@ function FlashCard(props) {
   switch (gameState) {
     case 'playing':
       return (
-        <div>
-          <ImageCard image={multipleChoices[correctIndex].image} />
-          <Button style={buttonStyle(0)} onClick={() => makeAGuess(0)}>{multipleChoices[0].taxonName}</Button>
-          <Button style={buttonStyle(1)} onClick={() => makeAGuess(1)}>{multipleChoices[1].taxonName}</Button>
-          <Button style={buttonStyle(2)} onClick={() => makeAGuess(2)}>{multipleChoices[2].taxonName}</Button>
+        <div className='row'>
+          <div className='col-xs-12 col-md-4'>
+            <ImageCard image={multipleChoices[correctIndex].image} />
+          </div>
+          <div className='col-xs-12 col-md-8'>
+            <p><Button style={buttonStyle(0)} onClick={() => makeAGuess(0)}>{multipleChoices[0].taxonName}</Button></p>
+            <p><Button style={buttonStyle(1)} onClick={() => makeAGuess(1)}>{multipleChoices[1].taxonName}</Button></p>
+            <p><Button style={buttonStyle(2)} onClick={() => makeAGuess(2)}>{multipleChoices[2].taxonName}</Button></p>
+          </div>
         </div>
       );
     case 'next':
       return (
-        <div>
-          <ImageCard image={multipleChoices[correctIndex].image} />
-          <p>Score: {props.score}</p>
-          <p>Yep, the answer is <a target="_blank" rel="noopener noreferrer" href={multipleChoices[correctIndex].item}>{multipleChoices[correctIndex].taxonName}</a>.</p>
-          <p><Button onClick={() => advanceToNextCard()} >Next</Button></p>
+        <div className='row'>
+          <div className='col-xs-12 col-md-4'>
+            <ImageCard image={multipleChoices[correctIndex].image} />
+          </div>
+          <div className='col-xs-12 col-md-8'>
+            <p>Score: {props.score}</p>
+            <p>Yep, the answer is <a target="_blank" rel="noopener noreferrer" href={multipleChoices[correctIndex].item}>{multipleChoices[correctIndex].taxonName}</a>.</p>
+            <p><Button onClick={() => advanceToNextCard()} >Next</Button></p>
+          </div>
         </div>
       );
     default:
@@ -44,7 +52,7 @@ function FlashCard(props) {
 function ImageCard(props) {
   const style = {
     background: `center / contain url(${props.image}?width=500)`,
-    width: '500px',
+    width: '100%',
     height: '500px',
   };
   
