b643328cf8278444a2d18c378e4f7e46fc433380
Lance W <50839441+disposableraft@users.noreply.github.com>
2019-07-03 11:56:47 -0400

    Sketch shape for new quiz

---
+++ src/data/brain-regions.js
@@ -0,0 +1,7 @@
+module.exports = [
+  {
+    region: "insular cortex",
+    description: "a portion of the cerebral cortex folded deep within the lateral sulcus (the fissure separating the temporal lobe from the parietal and frontal lobes)",
+    image: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Sobo_1909_633.png"
+  }
+]
